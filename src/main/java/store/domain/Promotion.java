0a318ff7c383aa05c3c8d8f6ad8e02c30e239b75
leeseongsil <leegwichan@naver.com>
2024-11-09 17:40:28 +0900

    feat(Promotion): 프로모션은 N개 구매 시 1개 무료 증정의 형태로 진행된다
    - 추후 NoPromotion을 위해 0개를 허용

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,10 +1,12 @@
 package store.domain;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 public class Promotion {
 
+
+    private static final int MIN_GET_COUNT = 0;
+    private static final int MAX_GET_COUNT = 1;
     private final String name;
     private final int buyCount;
     private final int getCount;
@@ -12,11 +14,26 @@ public class Promotion {
     private final LocalDate endDate;
 
     public Promotion(String name, int buyCount, int getCount, LocalDate startDate, LocalDate endDate) {
+        validateGetCount(getCount);
+        validateDates(startDate, endDate);
+
         this.name = name;
         this.buyCount = buyCount;
         this.getCount = getCount;
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    private void validateGetCount(int getCount) {
+        if (getCount < MIN_GET_COUNT || getCount > MAX_GET_COUNT) {
+            throw new IllegalArgumentException("get 개수는 %d와 %d 사이여야 합니다".formatted(MIN_GET_COUNT, MAX_GET_COUNT));
+        }
+    }
+
+    private void validateDates(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("시작 날짜는 끝 날짜보다 이전이거나 같아야 합니다");
+        }
     }
 
     public boolean canPromote(LocalDate currentDate) {
--- src/test/java/store/domain/PromotionTest.java
+++ src/test/java/store/domain/PromotionTest.java
@@ -1,12 +1,51 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PromotionTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 2})
+    void validateGetCountTest_whenGetCountIsOutOfRange_throwException(int getCount) {
+        assertThatThrownBy(() -> new Promotion("프로모션", 2, getCount, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 31)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("get 개수는 0와 1 사이여야 합니다");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void validateGetCountTest_whenGetCountIsInRange(int getCount) {
+        assertThatCode(() -> new Promotion("프로모션", 2, getCount, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 31)))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void validateDatesTest_whenStartDateIsAfterThanEndDate_throwException() {
+        LocalDate startDate = LocalDate.of(2024, 1, 2);
+        LocalDate endDate = LocalDate.of(2024, 1, 1);
+
+        assertThatThrownBy(() -> new Promotion("프로모션", 2, 1, startDate, endDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시작 날짜는 끝 날짜보다 이전이거나 같아야 합니다");
+    }
+
+    @Test
+    void validateDatesTest_whenStartDateIsBeforeOrEqualThanEndDate() {
+        LocalDate startDate = LocalDate.of(2024, 1, 1);
+        LocalDate endDate = LocalDate.of(2024, 1, 1);
+
+        assertThatCode(() -> new Promotion("프로모션", 2, 1, startDate, endDate))
+                .doesNotThrowAnyException();
+    }
+
 
     @ParameterizedTest
     @CsvSource({
