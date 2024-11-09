a3aa715c5f6cc7a76a3df416dbcc9ee9542c6458
leeseongsil <leegwichan@naver.com>
2024-11-09 16:30:19 +0900

    feat(Product): 고객이 상품을 구매할 때마다, 결제된 수량만큼 해당 상품의 재고에서 차감하여 수량을 관리한다
    - 재고 수량 초과 요청 시, 에러를 던짐

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -3,7 +3,7 @@ package store.domain;
 public class Product {
 
     private final String name;
-    private final int count;
+    private int count;
 
     public Product(String name, int count) {
         this.name = name;
@@ -12,5 +12,17 @@ public class Product {
 
     public boolean canBuy(int count) {
         return count <= this.count;
+    }
+
+    public void buy(int count) {
+        if (canBuy(count)) {
+            this.count -= count;
+            return;
+        }
+        throw new IllegalStateException("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    }
+
+    public int getCount() {
+        return count;
     }
 }
--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -1,6 +1,7 @@
 package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +23,24 @@ class ProductTest {
         boolean actual = product.canBuy(4);
 
         assertThat(actual).isFalse();
+    }
+
+    @Test
+    void buyTest_hasEqualOrMoreThanNeed() {
+        Product product = new Product("상품", 3);
+        int expectedCount = 0;
+
+        product.buy(3);
+
+        assertThat(product.getCount()).isEqualTo(expectedCount);
+    }
+
+    @Test
+    void buyTest_hasLessThanNeed_throwException() {
+        Product product = new Product("상품", 3);
+
+        assertThatThrownBy(() -> product.buy(4))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
     }
 }
