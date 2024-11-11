9152173f4660884bc12a6c1caf5ac5defbfb4307
leeseongsil <leegwichan@naver.com>
2024-11-11 18:43:25 +0900

    feat(InputView): 구매할 상품과 수량을 입력 받는다
    - 상품명, 수량은 하이픈(-)으로, 개별 상품은 대괄호([])로 묶어 쉼표(,)로 구분한다

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -2,10 +2,14 @@ package store.view;
 
 import static java.util.stream.Collectors.toMap;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import store.dto.ProductDto;
 import store.dto.PromotionDto;
 import store.dto.StoreDto;
@@ -17,6 +21,8 @@ public class InputView {
 
     private static final String TOKEN_DELIMITER = ",";
     private static final String INPUT_EMPTY_PROMOTION = "null";
+
+    private static final Pattern PRODUCTS_PATTERN = Pattern.compile("\\[([^]]+)-([0-9]+)]");
 
     private InputView() {
     }
@@ -70,11 +76,31 @@ public class InputView {
         });
     }
 
+    public static Map<String, Integer> inputPurchaseProducts() {
+        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        return toPurchasedProducts(readConsole());
+    }
+
+    private static Map<String, Integer> toPurchasedProducts(String text) {
+        Matcher matcher = PRODUCTS_PATTERN.matcher(text);
+        Map<String, Integer> result = new LinkedHashMap<>();
+        while (matcher.find()) {
+            String product = matcher.group(1);
+            int quantity = toInt(matcher.group(2));
+            result.put(product, quantity);
+        }
+        return result;
+    }
+
     private static int toInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해야 합니다 input : " + input, e);
         }
+    }
+
+    private static String readConsole() {
+        return Console.readLine();
     }
 }
